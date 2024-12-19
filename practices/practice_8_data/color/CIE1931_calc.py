216a1dec1831a2028f25450784c9f9e0bc61f8dc
qrspeter <qrspeter@gmail.com>
2024-12-19 14:13:25 +0800

    colors for specrta fixed

--- practices/practice_8_data/color/CIE1931_calc.py
+++ practices/practice_8_data/color/CIE1931_calc.py
@@ -10,15 +10,15 @@ spectra = 'led6p3_12v0008.ISD' # 11, 13, 21, 52
 csv_dir = './csv/'
 
 def integrate(f, step):
-    area = np.sum(cmf[:,f])*step
+    area = np.sum(f)*step
     return area  
     
     
 def rgbcalc(cmf, spectra, step):
     a = []
-    for i in range(1,4):
-        a = cmf[:,i]*spectra
-        a.append(integral(a, step))
+    for i in range(3):
+        c = cmf[i]*spectra
+        a.append(integrate(c, step))
         
     return tuple(a) 
     
@@ -31,9 +31,9 @@ def loadfiles(file_cmf,file_spect):
     y_sp_interp = np.interp(x_interp,x_sp_o,y_sp_o)
     y_cmf_o=cmf_o[:,0]
     y_cmf_interp=[]
-    for i in range(3):
-        y_cmf_interp.append(np.interp(x_interp,x_cmf_o,cmf_o[:,i+1]))
-    return(y_cmf_interp) # а вот y_sp_interp нигде не возвращается, то ли возвращать кортежем оба массива, то ли двумя разными функциями открывать.      
+    for i in range(1, 4):
+        y_cmf_interp.append(np.interp(x_interp,x_cmf_o,cmf_o[:,i]))
+    return(y_cmf_interp) # y_sp_interp returns nothing, I should return both arrays or use 2 different functions.      
     
 cmf_o = np.loadtxt(csv_dir + cmf, delimiter=',')
 sp_o = np.loadtxt(csv_dir + spectra, delimiter='\t', skiprows = 212)
@@ -43,9 +43,9 @@ y_sp_interp = np.interp(x_interp, x_sp_o, y_sp_o)
 x_cmf_o = cmf_o[:, 0]
 y_cmf_interp = []
 
-for i in range(3):
-    y_cmf_interp.append(np.interp(x_interp, x_cmf_o, cmf_o[:, i + 1]))
+for i in range(1, 4):
+    y_cmf_interp.append(np.interp(x_interp, x_cmf_o, cmf_o[:, i]))
 arr = rgbcalc(y_cmf_interp, y_sp_interp, step)
 
-RGB = np.array(arr) # если такая конвертация допустима
+RGB = np.array(arr) # not sure of it is correct
 cl.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(RGB)
